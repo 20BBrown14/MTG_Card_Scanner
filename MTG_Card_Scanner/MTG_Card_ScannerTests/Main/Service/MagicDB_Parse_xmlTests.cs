804c178723856bb104640193fc8296e4deced1a2
20bbrown14 <20bbrown14@gmail.com>
2018-08-12 19:38:23 -0500

    Added functionality to parse sets for database file

--- MTG_Card_Scanner/MTG_Card_ScannerTests/Main/Service/MagicDB_Parse_xmlTests.cs
+++ MTG_Card_Scanner/MTG_Card_ScannerTests/Main/Service/MagicDB_Parse_xmlTests.cs
@@ -25,7 +25,6 @@ namespace MTG_Card_Scanner.Tests
         [TestInitialize]
         public void SetUp()
         {
-            _magicDB_Parse_xml = new MagicDB_Parse_xml();
         }
 
         /// <summary>
@@ -34,12 +33,13 @@ namespace MTG_Card_Scanner.Tests
         [TestMethod()]
         public void MagicDB_Parse_xml_NoArg_Test()
         {
+            _magicDB_Parse_xml = new MagicDB_Parse_xml();
             Assert.IsNotNull(_magicDB_Parse_xml.xmlDatabase);
             Assert.IsNotNull(_magicDB_Parse_xml.cardList);
             Assert.IsFalse(String.IsNullOrEmpty(_magicDB_Parse_xml.xmlDatabasePath));
             Assert.AreEqual("../../Main/Resources/Database/MagicTG_DB_Main.xml", _magicDB_Parse_xml.xmlDatabasePath);
         }
-        
+
         /// <summary>
         /// Tests the parameterized constructor with valid inputs.
         /// </summary>
@@ -65,16 +65,29 @@ namespace MTG_Card_Scanner.Tests
         }
 
         /// <summary>
-        /// Tests the LoadDBTest method. Should return a non <code>null</code> 
+        /// Tests the LoadCards method. Should return a non <code>null</code> 
         /// List<Card> with a count of greater than one.
         /// </summary>
         [TestMethod()]
-        public void LoadDBTest()
+        public void LoadCardsTest()
         {
             _magicDB_Parse_xml = new MagicDB_Parse_xml("../../../Main/Resources/Database/MagicTG_DB_Main.xml");
-            List<Card> cardList = _magicDB_Parse_xml.LoadDB();
+            List<Card> cardList = _magicDB_Parse_xml.LoadCards();
             Assert.IsNotNull(cardList);
             Assert.AreNotEqual(0, cardList.Count);
+        }
+
+        /// <summary>
+        /// Tests the LoadSets method. Should return a non <code>null</code>
+        /// List<MTGSet> with a count of greater than one.
+        /// </summary>
+        [TestMethod()]
+        public void LoadSetsTest()
+        {
+            _magicDB_Parse_xml = new MagicDB_Parse_xml("../../../Main/Resources/Database/MagicTG_DB_Main.xml");
+            List<MTGSet> setList = _magicDB_Parse_xml.LoadSets();
+            Assert.IsNotNull(setList);
+            Assert.AreNotEqual(0, setList.Count);
         }
     }
 }
---
+++ MTG_Card_Scanner/Main/Models/MTGSet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MTG_Card_Scanner.CodeContract;
+
+namespace MTG_Card_Scanner
+{
+    /// <summary>
+    /// Model for an MTG card set
+    /// 
+    /// Author: Branden Brown
+    /// </summary>
+    public class MTGSet
+    {
+        public string setName { get; }
+
+        public string setCode { get; }
+
+        /// <summary>
+        /// Constructor for the class MTGSet.
+        /// </summary>
+        /// <param name="setName">string: The name of the set. Cannot be <code>null</code> or empty.</param>
+        /// <param name="setCode">string: The code of the set. Cannot be <code>null</code> or empty.</param>
+        public MTGSet(string setName, string setCode)
+        {
+            Requires<ArgumentNullException>(String.IsNullOrEmpty(setName), "Set name cannot be null or empty");
+            Requires<ArgumentNullException>(String.IsNullOrEmpty(setCode), "Set code cannot be null or empty");
+            this.setName = setName;
+            this.setCode = setCode;
+        }
+    }
+}
